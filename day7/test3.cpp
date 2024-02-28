3c0f4e0a0bb89029bdc2e128f922628aeaa69991
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-02-28 21:48:09 +0530

    Update test3.cpp

--- day7/test3.cpp
+++ day7/test3.cpp
@@ -1,7 +1,22 @@
+// Struct---------------
+
 #include<iostream>
 using namespace std;
+struct Rectangle
+{
+    int length;
+    int breadth;
+    char x; 
+}/* r1,r2,r3*/;
 
 int main(){
+    struct Rectangle r1={10,5};
+
+    cout<<"size of Rectangle r1 : "<<sizeof(r1)<<endl;
+    r1.length = 15;
+    r1.breadth = 7;
+    cout<<"length of Rectangle r1 : "<<r1.length<<endl;
+    cout<<"breadth of Rectangle r1 : "<<r1.breadth<<endl;
     
     return 0;
 }
