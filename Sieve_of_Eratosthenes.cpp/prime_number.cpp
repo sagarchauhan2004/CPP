4c97dc493dddb9ff8cdf8212cb67e71ef3291182
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-03-12 19:29:25 +0530

    Add files via upload

---
+++ Sieve_of_Eratosthenes.cpp/prime_number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+void primeSieve(int n)
+{
+    int prime[100] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i] == 0)
+        {
+            for (int j = i * i; j <= n; j =j + i)
+            {
+                prime[j] = 1;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i] == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "enter the number : ";
+    cin >> n;
+    primeSieve(n);
+    return 0;
+}
