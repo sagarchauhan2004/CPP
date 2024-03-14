6220396b4600a7bb1bdc7311e7292240d99618e2
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-03-14 20:33:15 +0530

    Add files via upload

---
+++ Two Pointer/Problem3.cpp
@@ -0,0 +1,55 @@
+// even integer on left side
+// odd integer on right side
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void sortbyparity(vector<int> &v)
+{
+    int left_ptr = 0;
+    int right_ptr = v.size() - 1;
+
+    while (left_ptr < right_ptr)
+    {
+        if (v[left_ptr] % 2 == 1 && v[right_ptr] % 2 == 0)
+        {
+            swap(v[left_ptr], v[right_ptr]);
+            left_ptr++;
+            right_ptr--;
+        }
+        if (v[left_ptr] % 2 == 0)
+        {
+            left_ptr++;
+        }
+        if (v[right_ptr] % 2 == 1)
+        {
+            right_ptr--;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cout << "enter the size : ";
+    cin >> n;
+
+    vector<int> v;
+    cout << "enter the element : ";
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin >> ele;
+        v.push_back(ele);
+    }
+
+    sortbyparity(v);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
